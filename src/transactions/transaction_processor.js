88ba8e93285bee518c0367466abdac2ca57eb460
Rachel Miles <rachmiles@gmail.com>
2022-03-03 14:31:31 +0000

    Pull arrow function out into a named arrow function to make it more readable

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,12 +1,10 @@
 function processTransactions(transactions) {
   const transactionCount = {};
-  transactions
-    .sort()
-    .forEach((transaction) =>
-      transactionCount[transaction]
-        ? (transactionCount[transaction] += 1)
-        : (transactionCount[transaction] = 1)
-    );
+  const incrementCount = (transaction) =>
+    transactionCount[transaction]
+      ? (transactionCount[transaction] += 1)
+      : (transactionCount[transaction] = 1);
+  transactions.sort().forEach((transaction) => incrementCount(transaction));
   return Object.entries(transactionCount)
     .sort((a, b) => b[1] - a[1])
     .map((element) => element.join(" "));
